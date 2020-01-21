62cb8ba89d94132096564e9330f656e04054469a
Gabriel Mussolin Lambert <gabriel.mussolin@gmail.com>
2020-01-21 16:34:50 -0300

    Configura o BD e cria o modelo de jogo

--- jogos/models.py
+++ jogos/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+MODALIDADE_CHOICES = [
+    ('FUT', 'futebol'),
+    ('BAS', 'basquete'),
+    ('TEN', 'tenis'),
+]
+
+class Jogo(models.Model):
+    jogador_01 = models.CharField(max_length=50)
+    jogador_02 = models.CharField(max_length=50)
+    torneio = models.CharField(max_length=100)
+    ano = models.CharField(max_length=4)
+    modalidade = models.CharField(choices=MODALIDADE_CHOICES, max_length=3)
+    imagem = models.ImageField(upload_to='fotos/%d/%m/%Y', blank=True)
--- streaming/settings.py
+++ streaming/settings.py
@@ -78,8 +78,11 @@ WSGI_APPLICATION = 'streaming.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'sportflix',
+        'USER': 'postgres',
+        'PASSWORD': 'chessegg91',
+        'HOST': 'localhost',
     }
 }
 
@@ -121,3 +124,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
