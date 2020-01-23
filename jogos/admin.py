529dee57662472ee9761b1612c7461854e95bff4
Gabriel Mussolin Lambert <gabriel.mussolin@gmail.com>
2020-01-23 10:31:10 -0300

    Cria paginacao, filtro e campo de busca no admin

--- jogos/admin.py
+++ jogos/admin.py
@@ -4,5 +4,8 @@ from .models import Jogo
 
 class ApresentandoJogos(admin.ModelAdmin):
     list_display = ('__str__', 'torneio', 'ano', 'modalidade')
+    list_per_page = 10
+    search_fields = ('jogador_01', 'jogador_02', )
+    list_filter = ('modalidade', 'ano', 'torneio', )
 
 admin.site.register(Jogo, ApresentandoJogos)
