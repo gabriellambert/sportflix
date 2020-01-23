5b565c0a9e30c4085112bfef9659fc3b8484aa5e
Gabriel Mussolin Lambert <gabriel.mussolin@gmail.com>
2020-01-23 13:49:05 -0300

    Adapta o template index por modalidade

--- jogos/views.py
+++ jogos/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render
 from .models import Jogo
 
 def index(request):
-    jogos_futebol = Jogo.objects.filter(modalidade__iexact='futebol')
-
+    jogos_futebol = Jogo.objects.filter(modalidade='FUT')[:4]
+    jogos_basquete = Jogo.objects.filter(modalidade='BAS')[:4]
+    jogos_tenis = Jogo.objects.filter(modalidade='TEN')[:4]
     context = {
         'jogos_futebol': jogos_futebol,
+        'jogos_basquete': jogos_basquete,
+        'jogos_tenis': jogos_tenis,
     }
     return render(request, 'index.html', context)
