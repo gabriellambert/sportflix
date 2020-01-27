f80abb9016772e1927ffd2b5a2f7514905b3a148
Gabriel Mussolin Lambert <gabriel.mussolin@gmail.com>
2020-01-27 16:15:31 -0300

    Adiciona modal de video (confirmar se ainda esta funcionando)

--- jogos/static/js/scripts.js
+++ jogos/static/js/scripts.js
@@ -7,3 +7,15 @@ function toggleFunction() {
         x.className = x.className.replace(" w3-show", "");
     }
 }
+
+$(function () {
+    $(".video").click(function () {
+        var theModal = $(this).data("target"),
+            videoSRC = $(this).attr("data-video"),
+            videoSRCauto = videoSRC + "?modestbranding=1&rel=0&controls=0&showinfo=0&html5=1&autoplay=1";
+        $(theModal + ' iframe').attr('src', videoSRCauto);
+        $(theModal + ' button.close').click(function () {
+            $(theModal + ' iframe').attr('src', videoSRC);
+        });
+    });
+});
